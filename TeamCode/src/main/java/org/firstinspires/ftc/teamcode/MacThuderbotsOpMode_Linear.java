504d90b2597204f15a4b6c95edadbc5ca2dbdcaf
thunderbotsteam <thunderbotsteam@gmail.com>
2019-11-23 18:04:11 -0800

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MacThuderbotsOpMode_Linear.java
@@ -0,0 +1,177 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * We took out @Disabled
+ * name="Basic: Linear OpMode"  changed to name="Basic: Thunderbots OpMode"
+ */
+
+@TeleOp(name="Basic: Mecanum Thunderbots TeleOp1", group="Thunderbots")
+
+public class MacThuderbotsOpMode_Linear extends LinearOpMode {
+
+    // Declare OpMode members.
+    MacHardwarePushbot robot   = new MacHardwarePushbot();   // Use a Pushbot's hardware
+    private ElapsedTime runtime = new ElapsedTime();
+
+    double leftForwardPower;
+    double rightForwardPower;
+    double leftBackwardPower;
+    double rightBackwardPower;
+
+
+
+   // private Servo grabber = null;
+    @Override
+    public void runOpMode() {
+
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        robot.init(hardwareMap);
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Status", "Init Done");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        //double tgtPower = 0;
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            // Setup a variable for each drive wheel to save power level for telemetry
+
+        driveMacChasis();
+        telemetry.update();
+
+        }
+
+
+
+    }
+    public void driveMacChasis(){
+        // POV Mode uses left stick to go forward, and right stick to turn.
+        // - This uses basic math to combine motions and is easier to drive straight.
+
+        double driveForward  =  gamepad1.right_stick_x;
+        double driveBackward  =  gamepad1.left_stick_x;
+        boolean driveRight = gamepad1.right_bumper;
+        boolean driveLeft = gamepad1.left_bumper;
+        double driveStop = gamepad1.right_trigger;
+
+        double powerMultiplier =0.5;
+
+
+
+
+        //Mecanum wheels work well with full power
+        if (driveRight) {
+            telemetry.addData("Status", "Moving right");
+            telemetry.update();
+
+            robot.leftDrive1.setPower(powerMultiplier);
+            robot.rightDrive1.setPower(-powerMultiplier);
+            robot.leftDrive2.setPower(-powerMultiplier);
+            robot.rightDrive2.setPower(powerMultiplier);
+
+        } else if (driveLeft) {
+
+            telemetry.addData("Status", "Moving left");
+            telemetry.update();
+
+            robot.leftDrive1.setPower(-powerMultiplier);
+            robot.rightDrive1.setPower(powerMultiplier);
+            robot.leftDrive2.setPower(powerMultiplier);
+            robot.rightDrive2.setPower(-powerMultiplier);
+        }
+        else if (driveForward > 0) {
+
+            telemetry.addData("Status", "Moving forward");
+            telemetry.update();
+
+            robot.leftDrive1.setPower(powerMultiplier);
+            robot.rightDrive1.setPower(powerMultiplier);
+            robot.leftDrive2.setPower(powerMultiplier);
+            robot.rightDrive2.setPower(powerMultiplier);
+        } else if (driveBackward > 0) {
+
+            telemetry.addData("Status", "Moving backward");
+            telemetry.update();
+
+            robot.leftDrive1.setPower(-powerMultiplier);
+            robot.rightDrive1.setPower(-powerMultiplier);
+            robot.leftDrive2.setPower(-powerMultiplier);
+            robot.rightDrive2.setPower(-powerMultiplier);
+        }
+        else if (driveStop>0) {
+            telemetry.addData("Status", "Stopping");
+            telemetry.update();
+
+            robot.leftDrive1.setPower(0);
+            robot.rightDrive1.setPower(0);
+            robot.leftDrive2.setPower(0);
+            robot.rightDrive2.setPower(0);
+        }
+
+        leftForwardPower= this.robot.leftDrive1.getPower();
+        rightForwardPower= this.robot.rightDrive1.getPower();
+        leftBackwardPower= this.robot.leftDrive1.getPower();
+        rightBackwardPower= this.robot.rightDrive1.getPower();
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Inputs received","Drive Forward: "+ driveForward + " Drive Backward: "+driveBackward+" Drive Right: "+ driveRight + " Drive Left: "+driveLeft);
+        telemetry.addData("Motors Forward", "left (%.2f), right (%.2f)", leftForwardPower, rightForwardPower);
+        telemetry.addData("Motors Backward", "left (%.2f), right (%.2f)", leftBackwardPower, rightBackwardPower);
+
+
+    }
+}
