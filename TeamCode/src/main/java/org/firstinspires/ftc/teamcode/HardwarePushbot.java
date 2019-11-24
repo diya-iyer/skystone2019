ef72e3f215c190a18ad2cdbd33f79d89ee8a7d1b
thunderbotsteam <thunderbotsteam@gmail.com>
2019-11-23 18:12:56 -0800

    Siva: Added a test comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -49,6 +49,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Motor channel:  Manipulator drive motor:  "left_arm"
  * Servo channel:  Servo to open left claw:  "left_hand"
  * Servo channel:  Servo to open right claw: "right_hand"
+ *
  */
 public class HardwarePushbot
 {
